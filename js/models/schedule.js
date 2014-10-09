da3dc5daf8d324b0fd91647a9bf8c3bf201dd326
Aarron Hulswitt <aarron@outerbox.com>
2014-10-08 20:47:20 -0400

    Updating the schedule and templates

--- js/models/schedule.js
+++ js/models/schedule.js
@@ -8,13 +8,6 @@ CustardPie.Schedule = DS.Model.extend({
 // This is a temp model for schedule 
 CustardPie.Schedule.FIXTURES = [
 	{
-		id: '3',
-		date: new Date(moment('8/23/2014')._i),
-		time: '8:00 pm',
-		venue: "Quaker Steak & Lube",
-		location: '5935 Canal Rd, Valley View, OH 44125'
-	},
-	{
 		id: '4',
 		date: new Date(moment('9/6/2014')._i),
 		time: '9:00 pm',
@@ -24,9 +17,64 @@ CustardPie.Schedule.FIXTURES = [
 	{
 		id: '5',
 		date: new Date(moment('9/13/2014')._i),
-		time: '9:00 pm',
+		time: '8:00 pm',
 		venue: 'Willowick Restaurant and Lounge',
 		location: '30609 Euclid Ave. Willowick, OH 44095'
-	}
-	
+	},
+	{
+		id: '6',
+		date: new Date(moment('9/27/2014')._i),
+		time: '9:00 pm',
+		venue: 'Wing Warehouse',
+		location: '10223 Northfield Rd, Northfield, OH 44067'
+	},
+	{
+		id: '7',
+		date: new Date(moment('10/4/2014')._i),
+		//time: '8:00 pm',
+		venue: 'Private Party',
+		location: 'Parma'
+	},
+	{
+		id: '8',
+		date: new Date(moment('10/18/2014')._i),
+		time: '9:00 pm',
+		venue: "Tripp's Bar & Grill",
+		location: '1100 W Royalton Rd, Broadview Hts., OH 44147'
+	},
+	{
+		id: '9',
+		date: new Date(moment('10/25/2014')._i),
+		time: '9:00 pm',
+		venue: "Mad Mike's Halloween Party",
+		location: '5045 Turney Rd, Cleveland, OH 44125'
+	},
+	{
+		id: '10',
+		date: new Date(moment('11/1/2014')._i),
+		time: '8:00 pm',
+		venue: 'The Bedford Eagles (Penny Lane)',
+		location: '237 Broadway Ave. Bedford, OH 44146'
+	},
+	{
+		id: '11',
+		date: new Date(moment('11/8/2014')._i),
+		time: '9:00 pm',
+		venue: 'Union House Bar & Restaurant',
+		location: '2713 Brookpark Rd, Cleveland, OH 44134'
+	},
+	{
+		id: '12',
+		date: new Date(moment('11/26/2014')._i),
+		time: '9:00 pm',
+		venue: 'Wing Warehouse',
+		location: '10223 Northfield Rd, Northfield, OH 44067'
+	},
+	{
+		id: '13',
+		date: new Date(moment('12/13/2014')._i),
+		time: '9:00 pm',
+		venue: "Tripp's Bar & Grill",
+		location: '1100 W Royalton Rd, Broadview Hts., OH 44147'
+	},
 ];
